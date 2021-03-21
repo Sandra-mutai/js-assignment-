c60367d4fa63a387b0fec5c5a55800b82fcb7d4e
sandra-mutai <sandramuttai@gmail.com>
2021-03-21 18:09:58 +0300

    assignment

---
+++ java.js
@@ -0,0 +1,20 @@
+function getGrades(){
+    var marks=89;
+
+if(marks>=80){
+    console.log("A");
+}
+else if(marks>=70){
+    console.log("B");
+}
+else if(marks>=60){
+    console.log("C");
+}
+else if(marks>=50){
+    console.log("D");
+}
+else if(marks<=49){
+    console.log("E");
+}
+}
+getGrades()
